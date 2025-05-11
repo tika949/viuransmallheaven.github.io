41c8f9a206e725a1adf05cfbde27136d1079d42a
tika949 <tikaram1022@gmail.com>
2025-05-11 22:18:32 +0545

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,19 @@
-
-const CACHE_NAME = "viuransmallheaven-cache-v1";
+const CACHE_NAME = "viuran-cache-v1";
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/styles.css",
-  "/script.js",  // तपाईंको स्क्रिप्ट नाम यहाँ राख्नुहोस्
-  "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png"
+  "/viuransmallheaven.github.io/",
+  "/viuransmallheaven.github.io/index.html",
+  "/viuransmallheaven.github.io/icons/icon-192x192.png",
+  "/viuransmallheaven.github.io/icons/icon-512x512.png"
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
